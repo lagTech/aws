edef85690126f4dce8e045f303f83118f4045e28
kevindeffo <deffokevin14@gmail.com>
2022-07-07 15:57:18 +0100

    some modification

--- substraction.py
+++ substraction.py
@@ -2,8 +2,7 @@
 nbr1= input("Enter the first number")
 nbr2= input("Enter the second number")
 def substraction(number1,number2):
-    result = nbr1 - nbr2
-    return result 
-
-#print(`{} - {} = {}`.format(nbr1,nbr2,substraction(nbr1,nbr2)))
-print(substraction(nbr2,nbr2))
+    resultat = nbr1 - nbr2
+    return resultat 
+resultat = substraction(nbr2,nbr2) 
+print(f'{nbr1} - {nbr2} = {resultat}')
