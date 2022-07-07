3892c5e478fa7b13e3e0877335fa153a483a7e40
noulapascal <noulapascal@gmail.com>
2022-07-07 15:31:07 +0100

    Le commit de Pascal sur la multiplication

---
+++ multiplication.py
@@ -0,0 +1,11 @@
+## Multiplication de deux nombre 
+##
+print('Bienvenue dans la calculatrice du test d\'AWS')
+numberOne = int(input('Entrez le premier nombre : '))
+numberTwo = int(input('Entrez le Second nombre : '))
+def multiplier(numberOne, nomberTwo):
+    result=numberOne * numberTwo
+    print('le résultat est ' + str(result))
+    return ()
+
+multiplier(numberOne, numberTwo)
