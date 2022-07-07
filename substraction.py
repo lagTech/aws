2795843811e6aef65e3920e66c6aee958bc38b5f
kevindeffo <deffokevin14@gmail.com>
2022-07-07 15:44:43 +0100

    substraction function

---
+++ substraction.py
@@ -0,0 +1,9 @@
+#this function ask two number to user and substract them
+nbr1= input("Enter the first number")
+nbr2= input("Enter the second number")
+def substraction(number1,number2):
+    result = nbr1 - nbr2
+    return result 
+
+#print(`{} - {} = {}`.format(nbr1,nbr2,substraction(nbr1,nbr2)))
+print(substraction(nbr2,nbr2))
